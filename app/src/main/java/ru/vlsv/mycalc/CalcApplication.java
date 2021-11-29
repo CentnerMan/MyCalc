e3e9bb421d2159cd73f25c3918ab6f7cd6c2df6a
Anatoly Lebedev <44133485+CentnerMan@users.noreply.github.com>
2021-11-29 16:59:40 +0300

    Add application class

---
+++ app/src/main/java/ru/vlsv/mycalc/CalcApplication.java
@@ -0,0 +1,11 @@
+package ru.vlsv.mycalc;
+
+import android.app.Application;
+
+public class CalcApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
